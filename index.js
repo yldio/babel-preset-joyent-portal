2df0884baa39a1be0c99c07e9c986b74186f2642
Sérgio Ramos <mail@sergioramos.me>
2017-12-13 10:53:22 +0000

    force inject minify if in production

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const cra = require('babel-preset-react-app')
+const cra = require('babel-preset-react-app');
 
-const { UMD } = process.env;
+const { UMD, NODE_ENV } = process.env;
+const isProduction = (NODE_ENV || '').toLowerCase() === 'production';
 
 module.exports = (ctx, opts) => {
   const _cra = Object.assign({}, cra);
 
-  if (Array.isArray(_cra.presets[0]) && _cra.presets[0][0].match(/babel-preset-env\/lib\/index\.js$/)) {
+  if (
+    Array.isArray(_cra.presets[0]) &&
+    _cra.presets[0][0].match(/babel-preset-env\/lib\/index\.js$/)
+  ) {
     // patch babel-preset-env
     _cra.presets[0][1] = Object.assign(_cra.presets[0][1], {
       modules: UMD ? 'umd' : false
@@ -13,14 +17,10 @@ module.exports = (ctx, opts) => {
   }
 
   return Object.assign({}, _cra, {
+    presets: isProduction ? _cra.presets.concat(['minify']) : _cra.presets,
     plugins: _cra.plugins.concat([
       require('babel-plugin-lodash'),
       require('babel-plugin-styled-components').default
-    ]),
-    env: Object.assign({}, _cra.env, {
-      production: {
-        presets: ['minify']
-      }
-    })
+    ])
   });
 };
